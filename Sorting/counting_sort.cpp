0c16d699eed369e2d3a534bd7c39f413fc04a867
Dylan Sloan <dylansloann2@gmail.com>
2021-02-15 22:46:05 -0600

    update radix / counting

--- Sorting/counting_sort.cpp
+++ Sorting/counting_sort.cpp
@@ -5,16 +5,19 @@ void countingSort(int* arr, int n, int k) {
     int* output = new int[n];
     int* count = new int[k+1];
     
+    // updates count (mid) array values
     for (i = 0; i <= k; i++) count[i] = 0;
     for (j = 0; j < n; j++) count[arr[j]]++;
     for (i = 1; i <= k; i++) count[i] += count[i - 1];
 
+    // updates output array while decrementing count
     for (j = n - 1; j >= 0; j--) {
         i = count[arr[j]] - 1;
         output[i] = arr[j];
         count[arr[j]]--;
     }
 
+    // copies values
     for (i = 0; i < n; i++) arr[i] = output[i]; 
 
     delete[] output;
--- Sorting/radix_sort.cpp
+++ Sorting/radix_sort.cpp
@@ -15,16 +15,19 @@ void radixSort(int* arr, int n, int d) {
 
     int i, j;
     for (int k = 0; k < d; k++) {
+        // updates count (mid) array values
         for (i = 0; i <= base; i++) count[i] = 0;
         for (i = 0; i < n; i++) count[getDigitAtIndex(arr[i], k)]++;
         for (i = 1; i <= base; i++) count[i] += count[i - 1];
 
+        // updates output array while decrementing count
         for (j = n - 1; j >= 0; j--) {
             i = count[getDigitAtIndex(arr[j], k)] - 1;
             output[i] = arr[j];
             count[getDigitAtIndex(arr[j], k)]--;
         }
 
+        // copies values to update order
         for (i = 0; i < n; i++) arr[i] = output[i];
     }
 
